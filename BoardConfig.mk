7f731d52131db1e13943ace85a4445a7ffc89798
xenxynon <flynryder427@gmail.com>
2025-02-25 06:58:47 +0000

    peridot-camera: merge device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+MIUICAMERA_PATH := device/xiaomi/peridot-camera
+
+# Inherit from the proprietary version
+include vendor/xiaomi/peridot-camera/BoardConfigVendor.mk
+
+# MiuiCamera
+CAMERA_PACKAGE_NAME := com.android.camera
+
+# Properties
+TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/private
